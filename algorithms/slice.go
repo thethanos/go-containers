0ca1ba52bd382cc4cb0d070415d7242f4c5ff5f3
Vadim Klimov <v.klimov@bastion-tech.ru>
2022-04-03 15:41:17 +0300

    changed some algorithm return values

--- algorithms/slice.go
+++ algorithms/slice.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-// counts all occurrences of the given value in the given slice
+// Counts all occurrences of the given value in the given slice.
 func Count[T comparable](data []T, value T) int {
 
 	res := 0
@@ -17,7 +17,7 @@ func Count[T comparable](data []T, value T) int {
 	return res
 }
 
-// counts all occurrences of values satisfying the given predicate in the given slice
+// Counts all occurrences of values satisfying the given predicate in the given slice.
 func CountIf[T comparable](data []T, predicate func(value T) bool) int {
 
 	res := 0
@@ -30,68 +30,64 @@ func CountIf[T comparable](data []T, predicate func(value T) bool) int {
 	return res
 }
 
-// finds the given value in the given slice
-// returns -1 if nothing found or the slice is empty
-func Find[T comparable](data []T, value T) int {
+// Finds the given value in the given slice, returns true if it is found.
+// Returns false if nothing found or the slice is empty.
+func Find[T comparable](data []T, value T) bool {
 
 	for i := range data {
 		if data[i] == value {
-			return i
+			return true
 		}
 	}
 
-	return -1
+	return false
 }
 
-// finds a value satisfying the given predicate in the given slice
-// returns -1 if nothing found or the slice is empty
-func FindIf[T any](data []T, predicate func(value T) bool) int {
+// Finds a value satisfying the given predicate in the given slice and returns it.
+// Returns defaultValue if nothing found or the slice is empty.
+func FindIf[T any](data []T, defaultValue T, predicate func(value T) bool) T {
 
-	for i := range data {
-		if predicate(data[i]) {
-			return i
+	for _, value := range data {
+		if predicate(value) {
+			return value
 		}
 	}
 
-	return -1
+	return defaultValue
 }
 
-// finds a maximal value in the given slice and returns its index
-// returns -1 if the slice is empty
-func MaxElement[T constraints.Ordered](data []T) int {
+// Finds a maximal value in the given slice and returns it.
+// Returns defaultValue if the slice is empty
+func MaxElement[T constraints.Ordered](data []T, defaultValue T) T {
 
 	if len(data) == 0 {
-		return -1
+		return defaultValue
 	}
 
-	maxIndex := -1
 	maxValue := data[0]
 	for i := range data {
 		if data[i] > maxValue {
-			maxIndex = i
 			maxValue = data[i]
 		}
 	}
 
-	return maxIndex
+	return defaultValue
 }
 
-// finds a minimal value in the given slice and returns its index
-// returns -1 if the slice is empty
-func MinElement[T constraints.Ordered](data []T) int {
+// Finds a minimal value in the given slice and returns it.
+// Returns defaultValue if the slice is empty
+func MinElement[T constraints.Ordered](data []T, defaultValue T) T {
 
 	if len(data) == 0 {
-		return -1
+		return defaultValue
 	}
 
-	minIndex := -1
 	minValue := data[0]
 	for i := range data {
 		if data[i] < minValue {
-			minIndex = i
 			minValue = data[i]
 		}
 	}
 
-	return minIndex
+	return minValue
 }
--- tests/algorithms/slice_test.go
+++ tests/algorithms/slice_test.go
@@ -6,44 +6,44 @@ import (
 	"github.com/thethanos/go-containers/algorithms"
 )
 
-type testDataInt struct {
+type testDataFindInt struct {
 	data   []int
 	value  int
-	result int
+	result bool
 }
 
-var testDataIntArray = []testDataInt{
-	{data: []int{0, 1, 2, 3, 4, 5, 6, 7}, value: 5, result: 5},
-	{data: []int{11, 12, 44, 77, 245, 3}, value: 22, result: -1},
-	{data: []int{}, value: 1, result: -1},
-}
-
-type testDataString struct {
-	data   []string
-	value  string
-	result int
-}
-
-var testDataStringArray = []testDataString{
-	{data: []string{"red", "green", "blue"}, value: "blue", result: 2},
-	{data: []string{"red", "green", "orange"}, value: "purple", result: -1},
-	{data: []string{}, value: "yellow", result: -1},
+var testDataFindIntArray = []testDataFindInt{
+	{data: []int{0, 1, 2, 3, 4, 5, 6, 7}, value: 5, result: true},
+	{data: []int{11, 12, 44, 77, 245, 3}, value: 22, result: false},
+	{data: []int{}, value: 1, result: false},
 }
 
 func TestFindInt(t *testing.T) {
 
-	for _, data := range testDataIntArray {
+	for _, data := range testDataFindIntArray {
 
 		res := algorithms.Find(data.data, data.value)
 		if res != data.result {
 			t.Error("result value does not match")
 		}
 	}
+}
+
+type testDataFindString struct {
+	data   []string
+	value  string
+	result bool
+}
+
+var testDataFindStringArray = []testDataFindString{
+	{data: []string{"red", "green", "blue"}, value: "blue", result: true},
+	{data: []string{"red", "green", "orange"}, value: "purple", result: false},
+	{data: []string{}, value: "yellow", result: false},
 }
 
 func TestFindString(t *testing.T) {
 
-	for _, data := range testDataStringArray {
+	for _, data := range testDataFindStringArray {
 
 		res := algorithms.Find(data.data, data.value)
 		if res != data.result {
@@ -52,22 +52,45 @@ func TestFindString(t *testing.T) {
 	}
 }
 
+type testDataFindIfInt struct {
+	data   []int
+	result int
+}
+
+var testDataFindIfIntArray = []testDataFindIfInt{
+	{data: []int{0, 1, 2, 3, 4, 5, 6, 7}, result: 5},
+	{data: []int{11, 12, 44, 77, 245, 3}, result: -1},
+	{data: []int{}, result: -1},
+}
+
 func TestFindIfInt(t *testing.T) {
 
-	for _, data := range testDataIntArray {
+	for _, data := range testDataFindIfIntArray {
 
-		res := algorithms.FindIf(data.data, func(value int) bool { return value > 4 && value < 6 })
+		res := algorithms.FindIf(data.data, -1, func(value int) bool { return value > 4 && value < 6 })
 		if res != data.result {
 			t.Error("result value does not match")
 		}
 	}
 }
 
+type testDataFindIfString struct {
+	data   []string
+	value  string
+	result string
+}
+
+var testDataFindIfStringArray = []testDataFindIfString{
+	{data: []string{"red", "green", "blue"}, result: "blue"},
+	{data: []string{"red", "green", "orange"}, result: "not found"},
+	{data: []string{}, result: "not found"},
+}
+
 func TestFindIfString(t *testing.T) {
 
-	for _, data := range testDataStringArray {
+	for _, data := range testDataFindIfStringArray {
 
-		res := algorithms.FindIf(data.data, func(value string) bool { return len(value) == 4 })
+		res := algorithms.FindIf(data.data, "not found", func(value string) bool { return len(value) == 4 })
 		if res != data.result {
 			t.Error("result value does not match")
 		}
